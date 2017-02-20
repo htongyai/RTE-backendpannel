53fcf014877a04ae9ebd6465f75ec0ea7b150691
Hariras Tongyai <mlhariras.tongyai@gmail.com>
2017-02-20 00:33:13 +0000

    first upload

---
+++ rte backpannel/web/index.php
@@ -0,0 +1,430 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Home </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Pooled Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/morris.css" type="text/css"/>
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<script src="js/jquery-2.1.4.min.js"></script>
+<!-- //jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+</head> 
+<body>
+   <div class="page-container">
+   <!--/content-inner-->
+<div class="left-content">
+	   <div class="mother-grid-inner">
+             <!--header start here-->
+				<div class="header-main">
+					<div class="logo-w3-agile">
+								<h1><a href="index.html">Royal Thai Embassy backend Pannel</a></h1>
+							</div>
+				
+						
+							
+				     <div class="clearfix"> </div>	
+				</div>
+<!--heder end here-->
+		<ol class="breadcrumb">
+                
+            </ol>
+<!--four-grids here-->
+		<div class="four-grids">
+					<div class="col-md-3 four-grid">
+						<div class="four-agileits">
+							<div class="icon">
+								<i class="glyphicon glyphicon-user" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Site Visit</h3>
+								<h4> Fetching from analytics  </h4>
+								
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3 four-grid">
+						<div class="four-agileinfo">
+							<div class="icon">
+								<i class="glyphicon glyphicon-list-alt" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Bookings</h3>
+								<h4>Fetching from analytics</h4>
+
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3 four-grid">
+						<div class="four-w3ls">
+							<div class="icon">
+								<i class="glyphicon glyphicon-folder-open" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Unused</h3>
+								<h4>-</h4>
+								
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3 four-grid">
+						<div class="four-wthree">
+							<div class="icon">
+								<i class="glyphicon glyphicon-briefcase" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Pannel Version</h3>
+								<h4>1.5</h4>
+								
+							</div>
+							
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+<!--//four-grids here-->
+<!--agileinfo-grap-->
+<div class="agileinfo-grap">
+<div class="agileits-box">
+<header class="agileits-box-header clearfix">
+<h3>Statistics - Fetching from analytics</h3>
+	<div class="toolbar">
+		<div class="pull-left">
+			<div class="btn-group">
+				<a href="#" class="btn btn-default btn-xs">Daily</a>
+				<a href="#" class="btn btn-default btn-xs active">Monthly</a>
+				<a href="#" class="btn btn-default btn-xs">Yearly</a>
+			</div>
+		</div>
+		<div class="pull-right">
+			<div class="btn-group">
+			  <a aria-expanded="false" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+				Export <i class="fa fa-angle-down"></i>
+			  </a>
+			  <ul class="dropdown-menu pull-right" role="menu">
+				<li><a href="#">Export as PDF</a></li>
+				<li><a href="#">Export as CSV</a></li>
+				<li><a href="#">Export as PNG</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+			  </ul>
+			</div>
+			<a href="#" class="btn btn-primary btn-xs"><i class="fa fa-cog"></i></a>
+		</div>
+	</div>
+</header>
+<div class="agileits-box-body clearfix">
+<div id="hero-area"></div>
+</div>
+</div>
+</div>
+	<!--//agileinfo-grap-->
+<!--photoday-section-->	
+			
+                        
+                    	
+                      	<div class="col-md-4 wthree-pan">
+			<div class="panel panel-default">
+			  <div class="panel-heading"> <i class="fa fa-bell fa-fw"></i> Update Pannel </div>
+			  <!-- /.panel-heading -->
+			  <div class="panel-body">
+				<div class="list-group"> 
+						<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 	<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 	<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 	<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 	<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 
+					<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Update <span class="pull-right text-muted small"><em></em> </span> </a> 
+				
+				</div>
+				<!-- /.list-group --> 
+				
+			  </div>
+			  <!-- /.panel-body --> 
+			</div>
+		  </div>
+                        </div>
+                        
+                           	<div class="col-md-4 wthree-pan">
+			<div class="panel panel-default">
+			  <div class="panel-heading"> <i class="fa fa-bell fa-fw"></i> Warning Pannel </div>
+			  <!-- /.panel-heading -->
+			  <div class="panel-body">
+				<div class="list-group"> 
+						<a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> *DO NOT USE FIRE FOX before Saturday 18th Febuary* on the back-end system (WILL CAUSE DATA TO OVERWRITE)<span class="pull-right text-muted small"><em></em> </span> </a> 	
+                    <a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Overflowed 20/02/17 - 42 Bookings + Overwritten <span class="pull-right text-muted small"><em></em> </span> </a> 	
+                    <a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Firefox bug - Booking Date: 20/02/17 <span class="pull-right text-muted small"><em></em> </span> </a> 	
+                    <a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Overflowed 20/02/17 - 38 Bookings + Overwritten <span class="pull-right text-muted small"><em></em> </span> </a> 	
+                    <a href="#" class="list-group-item"> <i class="fa fa-bolt fa-fw"></i> System Firefox bug - Booking Date: 15/02/17 <span class="pull-right text-muted small"><em></em> </span> </a> 
+					
+				
+				</div>
+				<!-- /.list-group --> 
+				
+			  </div>
+			  <!-- /.panel-body --> 
+			</div>
+		  </div>
+                    	<div class="col-sm-4 w3-agile-crd">
+                            <div class="card">
+                                <div class="card-body card-padding">
+                                    <div class="widget">
+                                        <header class="widget-header">
+                                            <h4 class="widget-title">Activities</h4>
+                                        </header>
+                                        <hr class="widget-separator">
+                                        <div class="widget-body">
+                                            <div class="streamline">
+                                                <div class="sl-item sl-primary">
+                                                    <div class="sl-content">
+                                                        <small class="text-muted">15/02/2017</small>
+                                                        <p>Incident: Firefox System overflow</p>
+                                                    </div>
+                                                </div>
+                                                <div class="sl-item sl-danger">
+                                                    <div class="sl-content">
+                                                        <small class="text-muted">01/02/2017</small>
+                                                        <p>Incident: Mobile Consular Date Error</p>
+                                                    </div>
+                                                </div>
+                                                <div class="sl-item sl-success">
+                                                    <div class="sl-content">
+                                                        <small class="text-muted">30/12/2916</small>
+                                                        <p>Incident: Double Booking migration</p>
+                                                    </div>
+                                                </div>
+                                              
+                                                
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						<div class="clearfix"></div>
+                   
+	<!--//photoday-section-->	
+	<!--w3-agileits-pane-->	
+	<div class="w3-agileits-pane">
+		
+		<div class="col-md-111 agile-info-stat">
+			<div class="stats-info stats-last widget-shadow">
+						<table class="table stats-table ">
+							<thead>
+								<tr>
+									<th>S.NO</th>
+									<th>Booking</th>
+									<th>STATUS</th>
+									<th>Version</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Passport Booking</td>
+									<td><span class="label label-success">Online</span></td>
+									<td><h5>1.5 <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>Thai ID Booking</td>
+								<td><span class="label label-success">Online</span></td>
+									<td><h5>1.5 <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>Mobile Consular Booking</td>
+									<td><span class="label label-success">Online</span></td>
+									<td><h5 class="down">1.3 <i class="fa fa-level-down"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">4</th>
+									<td>Analytics</td>
+									<td><span class="label label-success">Online</span></td>
+									<td><h5>Google Update <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">5</th>
+									<td>RSVP System</td>
+                                    <td><span class="label label-warning">In Progress</span></td>
+									<td><h5 class="down">1.3 </h5></td>
+								</tr>
+								<tr>
+									<th scope="row">6</th>
+									<td>Website</td>
+								<td><span class="label label-success">Online</span></td>
+									<td><h5>3.0 <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+			</div>
+		  <div class="clearfix"></div>
+	  </div>
+	  <!--//w3-agileits-pane-->	
+<!-- script-for sticky-nav -->
+		<script>
+		$(document).ready(function() {
+			 var navoffeset=$(".header-main").offset().top;
+			 $(window).scroll(function(){
+				var scrollpos=$(window).scrollTop(); 
+				if(scrollpos >=navoffeset){
+					$(".header-main").addClass("fixed");
+				}else{
+					$(".header-main").removeClass("fixed");
+				}
+			 });
+			 
+		});
+		</script>
+		<!-- /script-for sticky-nav -->
+<!--inner block start here-->
+<div class="inner-block">
+
+</div>
+<!--inner block end here-->
+<!--copy rights start here-->
+<div class="copyrights">
+	 <p>© 2017All Rights Reserved |  </p>
+</div>	
+<!--COPY rights end here-->
+</div>
+</div>
+  <!--//content-inner-->
+			<!--/sidebar-menu-->
+				<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="index.html"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+										
+										 <li id="menu-academico" ><a href="email.html"><i class="fa fa-envelope nav_icon"></i><span>Email</span><div class="clearfix"></div></a></li>
+									<li><a href="adminmain.html"><i class="fa fa-picture-o" aria-hidden="true"></i><span>Appointments</span><div class="clearfix"></div></a></li>
+                                        <li><a href="mobileconsular.html"><i class="fa fa-picture-o" aria-hidden="true"></i><span>Mobile Consular</span><div class="clearfix"></div></a></li>
+									<!--<li id="menu-academico" ><a href="charts.html"><i class="fa fa-bar-chart"></i><span>National ID</span><div class="clearfix"></div></a></li>-->
+									
+									<li id="menu-academico" ><a href="holiday.html"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i><span>Holiday Calendar</span><div class="clearfix"></div></a></li>
+									<!--  <li id="menu-academico" ><a href="#"><i class="fa fa-cogs" aria-hidden="true"></i><span> UI Components</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   <li id="menu-academico-avaliacoes" ><a href="button.html">Buttons</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="grid.html">Grids</a></li>
+										  </ul>
+										</li>-->
+									 <li><a href="https://analytics.google.com/analytics/web/?authuser=1#report/defaultid/a30301754w132953703p136933775/"><i class="fa fa-table"></i>  <span>Analytics</span><div class="clearfix"></div></a></li>
+									<li><a href="https://us12.admin.mailchimp.com/"><i class="fa fa-map-marker" aria-hidden="true"></i>  <span>Mailchimp</span><div class="clearfix"></div></a></li>
+							       
+                                   <li><a href="soon.html"><i class="fa fa-table"></i>  <span>RSVP</span><div class="clearfix"></div></a></li>
+									
+								  </ul>
+								</div>
+							  </div>
+							  <div class="clearfix"></div>		
+							<script>
+							var toggle = true;
+										
+							$(".sidebar-icon").click(function() {                
+							  if (toggle)
+							  {
+								$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+								$("#menu span").css({"position":"absolute"});
+							  }
+							  else
+							  {
+								$(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+								setTimeout(function() {
+								  $("#menu span").css({"position":"relative"});
+								}, 400);
+							  }
+											
+											toggle = !toggle;
+										});
+							</script>
+<!--js -->
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+   <!-- /Bootstrap Core JavaScript -->	   
+<!-- morris JavaScript -->	
+<script src="js/raphael-min.js"></script>
+<script src="js/morris.js"></script>
+<script>
+	$(document).ready(function() {
+		//BOX BUTTON SHOW AND CLOSE
+	   jQuery('.small-graph-box').hover(function() {
+		  jQuery(this).find('.box-button').fadeIn('fast');
+	   }, function() {
+		  jQuery(this).find('.box-button').fadeOut('fast');
+	   });
+	   jQuery('.small-graph-box .box-close').click(function() {
+		  jQuery(this).closest('.small-graph-box').fadeOut(200);
+		  return false;
+	   });
+	   
+	    //CHARTS
+	    function gd(year, day, month) {
+			return new Date(year, month - 1, day).getTime();
+		}
+		
+		graphArea2 = Morris.Area({
+			element: 'hero-area',
+			padding: 10,
+        behaveLikeLine: true,
+        gridEnabled: false,
+        gridLineColor: '#dddddd',
+        axes: true,
+        resize: true,
+        smooth:true,
+        pointSize: 0,
+        lineWidth: 0,
+        fillOpacity:0.85,
+			data: [
+				{period: '2014 Q1', iphone: 2668, ipad: null, itouch: 2649},
+				{period: '2014 Q2', iphone: 15780, ipad: 13799, itouch: 12051},
+				{period: '2014 Q3', iphone: 12920, ipad: 10975, itouch: 9910},
+				{period: '2014 Q4', iphone: 8770, ipad: 6600, itouch: 6695},
+				{period: '2015 Q1', iphone: 10820, ipad: 10924, itouch: 12300},
+				{period: '2015 Q2', iphone: 9680, ipad: 9010, itouch: 7891},
+				{period: '2015 Q3', iphone: 4830, ipad: 3805, itouch: 1598},
+				{period: '2015 Q4', iphone: 15083, ipad: 8977, itouch: 5185},
+				{period: '2016 Q1', iphone: 10697, ipad: 4470, itouch: 2038},
+				{period: '2016 Q2', iphone: 8442, ipad: 5723, itouch: 1801}
+			],
+			lineColors:['#ff4a43','#a2d200','#22beef'],
+			xkey: 'period',
+            redraw: true,
+            ykeys: ['iphone', 'ipad', 'itouch'],
+            labels: ['All Visitors', 'Returning Visitors', 'Unique Visitors'],
+			pointSize: 2,
+			hideHover: 'auto',
+			resize: true
+		});
+		
+	   
+	});
+	</script>
+</body>
+</html>
